122bc31ce7178e376989cd0b984166a5aff7630d
yuvarajkcp <yuvakcp@gmail.com>
2023-09-26 21:34:49 -0100

    first user commit

---
+++ A.java
@@ -0,0 +1,9 @@
+class A
+{
+	public static void main(String[] args)
+	{
+		System.out.println("i am first user");
+		System.out.println("i am first user");
+		System.out.println("i am first user");
+	}
+}
